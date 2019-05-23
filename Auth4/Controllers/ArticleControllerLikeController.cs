a14cb63d32aca25ddecdb2eb5eb9960f34c68ce9
unknown <zakaria.hajlaoui@gmail.com>
2019-05-23 23:54:08 +0100

    xd

--- Auth4/Controllers/ArticleControllerLikeController.cs
+++ Auth4/Controllers/ArticleControllerLikeController.cs
@@ -13,7 +13,7 @@ namespace Auth4.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Like([Bind("ArticleId,ArticleTitle,desc_mini,desc,ArticleAdress,Articlecoor,ArticleContact,ImagePath,ImageName,AuthorId,AuthorName,FlagCount,Status")] Article article,int id)
         {
-
+            return View();
         }
     }
 }
