98626609b369025472003432dfb5247f30a543d9
unknown <zakaria.hajlaoui@gmail.com>
2019-05-29 14:39:27 +0100

    comments load faster

--- Auth4/Controllers/ArticleControllerDetails.cs
+++ Auth4/Controllers/ArticleControllerDetails.cs
@@ -1,4 +1,5 @@
-﻿using BrightPathDev.ViewModels;
+﻿using BrightPathDev.Models;
+using BrightPathDev.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -26,9 +27,17 @@ namespace BrightPathDev.Controllers
             var likes = _context.LikeModels.ToList();
             var dislikes = _context.DislikeModels.ToList();
             var comments = _context.Comments.ToList();
+            var cmnts = new List<Comment>();
+            foreach(var item in comments)
+            {
+                if (item.ArticleId == id)
+                {
+                    cmnts.Add(item);
+                }
+            }
             articleViewModel.LikeModels = likes;
             articleViewModel.DislikeModels = dislikes;
-            articleViewModel.Comments = comments;
+            articleViewModel.Comments = cmnts;
             if (articleViewModel.Article == null)
             {
                 return NotFound();
